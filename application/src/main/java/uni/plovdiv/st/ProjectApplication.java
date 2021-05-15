9429eb1b5d59aa4dfe1239a3bd9dd00ecce01049
Vitali Atias <vitaliatias@gmail.com>
2021-05-15 14:19:02 +0300

    Fix about heroku.

--- application/src/main/java/uni/plovdiv/st/ProjectApplication.java
+++ application/src/main/java/uni/plovdiv/st/ProjectApplication.java
@@ -2,9 +2,17 @@ package uni.plovdiv.st;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class ProjectApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Software tools :)";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
