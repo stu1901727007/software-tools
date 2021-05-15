cc8b307e3569b442a5dc366a596a1a16ca0c163e
Vitali Atias <vitaliatias@gmail.com>
2021-05-15 12:44:22 +0300

    ModuleD has been created

---
+++ moduleD/src/main/java/ModuelDApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuelDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuelDApplication.class, args);
+    }
+
+}
